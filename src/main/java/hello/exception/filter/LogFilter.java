08e2b7496faf5aa530dc4232c687d0b6fe6db8ff
taeho <dlxogh439@gmail.com>
2024-09-23 22:31:58 +0900

    Filter & Interceptor를 활용한 Exception 처리

---
+++ src/main/java/hello/exception/filter/LogFilter.java
@@ -0,0 +1,39 @@
+package hello.exception.filter;
+
+import lombok.extern.slf4j.Slf4j;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.UUID;
+
+@Slf4j
+public class LogFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        log.info("LogFilter init");
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest) servletRequest;
+        String requestURI = httpRequest.getRequestURI();
+        String uuid = UUID.randomUUID().toString();
+
+        try {
+            log.info("REQUEST [{}][{}][{}]", uuid, httpRequest.getDispatcherType(), requestURI);
+            filterChain.doFilter(servletRequest, servletResponse);
+        } catch (Exception e) {
+            log.info("EXCEPTION {}", e.getMessage());
+            throw e;
+        } finally {
+            log.info("RESPONSE [{}][{}][{}]", uuid, httpRequest.getDispatcherType(), requestURI);
+        }
+    }
+
+    @Override
+    public void destroy() {
+        log.info("log filter destroy");
+    }
+}
